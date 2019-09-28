6942bf3957c8946c0e3089ad097f34d3599a232d
PlusLibrary <42900821+PlusLibrary@users.noreply.github.com>
2019-09-28 23:09:44 +0900

    First commit

---
+++ javascript.js
@@ -0,0 +1,51 @@
+var hornName = [
+	"Yamaha YHR-871D",
+	"J.Michael FH-850",
+	"J.Michael PFH-550S",
+	"Alexander Double Horn 103",
+	"Hans Hoyer 3702",
+];
+
+function add(n)
+{
+	return document.createElement(n);
+}
+
+function addLabel(type, content)
+{
+	var e = add(type);
+	e.innerText = content;
+	return e;
+}
+
+function addComma(i)
+{
+	return i.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function gen()
+{
+	var obj = add("obj");
+	var random = Math.floor(Math.random() * hornName.length);
+	console.log(hornName.length);
+
+	var img = add("imgBox");
+	img.style.backgroundImage = "url('horn/horn" + random + ".png')";
+	obj.appendChild(img);
+	obj.appendChild(addLabel("name", hornName[random]));
+	obj.appendChild(addLabel("view", addComma(Math.floor(Math.random() * 32767)) + " views"));
+	obj.appendChild(addLabel("like", addComma(90 + Math.floor(Math.random() * 10)) + "%"));
+
+	document.getElementById("content").appendChild(obj);
+}
+
+function main()
+{
+	var count = Math.floor(Math.random() * 16) + 10;
+	for(var i = 0; i < count; i++)
+	{
+		gen();
+	}
+}
+
+main();
