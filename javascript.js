d246d593f272aa74b2605fe86b8276d42d5a0e20
Plus Library <cpluslib@gmail.com>
2021-01-03 23:42:06 +0900

    switch coding style

--- javascript.js
+++ javascript.js
@@ -1,34 +1,30 @@
-var hornName = [
-	"Yamaha YHR-871D",
-	"J.Michael FH-850",
-	"J.Michael PFH-550S",
-	"Alexander Double Horn 103",
-	"Hans Hoyer 3702",
+const hornName = [
+    "Yamaha YHR-871D",
+    "J.Michael FH-850",
+    "J.Michael PFH-550S",
+    "Alexander Double Horn 103",
+    "Hans Hoyer 3702",
 ];
 
-function add(n)
-{
-	return document.createElement(n);
+function add(n) {
+    return document.createElement(n);
 }
 
-function addLabel(type, content)
-{
-	var e = add(type);
-	e.innerText = content;
-	return e;
+function addLabel(type, content) {
+    let e = add(type);
+    e.innerText = content;
+    return e;
 }
 
-function addComma(i)
-{
+function addComma(i) {
 	return i.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
-function gen()
-{
-	var obj = add("obj");
-	var random = Math.floor(Math.random() * hornName.length);
+function gen() {
+	let obj = add("obj");
+	let random = Math.floor(Math.random() * hornName.length);
 
-	var img = add("imgBox");
+	let img = add("imgBox");
 	img.style.backgroundImage = "url('horn/horn" + random + ".png')";
 	obj.appendChild(img);
 	obj.appendChild(addLabel("name", hornName[random]));
@@ -38,11 +34,9 @@ function gen()
 	document.getElementById("content").appendChild(obj);
 }
 
-function main()
-{
-	var count = Math.floor(Math.random() * 16) + 10;
-	for(var i = 0; i < count; i++)
-	{
+function main() {
+	let count = Math.floor(Math.random() * 16) + 10;
+	for(let i = 0; i < count; i++) {
 		gen();
 	}
 }
