0fef23dd4094e6431f59df241a0c1c2d8536f694
Plus Library <cpluslib@gmail.com>
2021-01-04 14:31:45 +0900

    refresh coding style

--- javascript.js
+++ javascript.js
@@ -1,44 +1,37 @@
-const hornName = [
-    "Yamaha YHR-871D",
-    "J.Michael FH-850",
-    "J.Michael PFH-550S",
-    "Alexander Double Horn 103",
-    "Hans Hoyer 3702",
-];
+(() => {
+    const horns = [
+        "Yamaha YHR-871D",
+        "J.Michael FH-850",
+        "J.Michael PFH-550S",
+        "Alexander Double Horn 103",
+        "Hans Hoyer 3702",
+    ];
 
-function add(n) {
-    return document.createElement(n);
-}
+    const add = n => document.createElement(n);
+    const addLabel = (type, content) => {
+        let e = add(type);
+        e.innerText = content;
+        return e;
+    };
+    
+    const main = () => {
+        const addComma = i => i.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    
+        [...Array((Math.floor(Math.random() * 4) + 4) * 3).keys()]
+            .map(() => Math.floor(Math.random() * horns.length))
+            .forEach(randomHorn => {
+                let obj = add("obj");
+                let img = add("imgBox");
+                img.style.backgroundImage = "url('horn/horn" + randomHorn + ".png')";
+                obj.appendChild(img);
+                obj.appendChild(addLabel("name", horns[randomHorn]));
+                obj.appendChild(addLabel("view", addComma(Math.floor(Math.random() * 32767)) + " views"));
+                obj.appendChild(addLabel("like", addComma(90 + Math.floor(Math.random() * 10)) + "%"));
+                document.getElementById("content").appendChild(obj);
+            });
 
-function addLabel(type, content) {
-    let e = add(type);
-    e.innerText = content;
-    return e;
-}
-
-function addComma(i) {
-	return i.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
-
-function gen() {
-	let obj = add("obj");
-	let random = Math.floor(Math.random() * hornName.length);
-
-	let img = add("imgBox");
-	img.style.backgroundImage = "url('horn/horn" + random + ".png')";
-	obj.appendChild(img);
-	obj.appendChild(addLabel("name", hornName[random]));
-	obj.appendChild(addLabel("view", addComma(Math.floor(Math.random() * 32767)) + " views"));
-	obj.appendChild(addLabel("like", addComma(90 + Math.floor(Math.random() * 10)) + "%"));
-
-	document.getElementById("content").appendChild(obj);
-}
-
-function main() {
-	let count = (Math.floor(Math.random() * 4) + 4) * 3;
-	for(let i = 0; i < count; i++) {
-		gen();
-	}
-}
-
-main();
+        document.getElementById("search").setAttribute("placeholder", `Search ${ Math.floor(Math.random() * 32767) } hornography...`);
+    }
+    
+    main();
+})();
